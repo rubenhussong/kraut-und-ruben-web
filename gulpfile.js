9db759236368095db7a649c09f5c839b0f1b2fd9
Ruben Hussong <rubenhussong@gmail.com>
2018-04-13 21:44:22 +0200

    gulp functions
    – minifyCss just in Build not in Prev
    – convertHtml in Build
    – convertJs in Build

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
 // How to install a plugin: sudo npm install gulp-* -D --unsafe-perm=true
 
 var gulp = require('gulp'),
+    gulpIf = require('gulp-if'),
+
+    // CSS
     sass = require('gulp-sass'),
     concatCss = require('gulp-concat-css'),
     minifyCss = require('gulp-minify-css'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
 
+    // HTML
+    minifyHtml = require('gulp-minify-html'),
 
+    // JS
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
 
 
 /*
-minifyHtml = require('gulp-minify-html'),
 imageMin = require('gulp-imagemin'),
 svgMin = require('gulp-svgmin');
 jshint = require('gulp-jshint'),
-uglify = require('gulp-uglify'),
 imagemin = require('gulp-imagemin'),
-rename = require('gulp-rename'),
 clean = require('gulp-clean'),
-concat = require('gulp-concat'),
 notify = require('gulp-notify'),
 cache = require('gulp-cache'),
 livereload = require('gulp-livereload'),
@@ -28,12 +32,18 @@ server = lr();
 
 // ============================================================ MAIN FUNCTIONS
 
+var currentTask;
+
 gulp.task('prev', function(){
-    convertSass('app/css');
+    currentTask = this.seq.slice(-1)[0];
+    convertSass('app');
 });
 
 gulp.task('build', function(){
-    convertSass('dist/css');
+    currentTask = this.seq.slice(-1)[0];
+    convertSass('dist');
+    convertHtml('dist');
+    convertJs('dist');
 });
 
 
@@ -42,10 +52,23 @@ gulp.task('build', function(){
 // ============================================================ SMALL FUNCTIONS
 
 function convertSass(destination){
-   return gulp.src('app/scss/**/*.scss')
-       .pipe(sass().on('error', sass.logError))
-       .pipe(autoprefixer({ browsers: ['IE 6','Chrome 9', 'Firefox 14']})) // create browserlist config file
-       .pipe(concatCss('style.css'))
-       .pipe(minifyCss())
-       .pipe(gulp.dest(destination));
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({ browsers: ['IE 6','Chrome 9', 'Firefox 14']})) // create browserlist config file
+        .pipe(concatCss('style.css'))
+        .pipe(gulpIf(currentTask == 'build', minifyCss())) // just in build-task
+        .pipe(gulp.dest(destination + '/css'));
+}
+
+function convertHtml(destination){
+    return gulp.src('app/index.html')
+        .pipe(minifyHtml())
+        .pipe(gulp.dest(destination));
+}
+
+function convertJs(destination){
+    return gulp.src('app/js/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(destination + 'js'));
 }
