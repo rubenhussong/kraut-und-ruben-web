93cb5d5d9d67a4a5d500057569aeee8869d61721
Ruben Hussong <rubenhussong@gmail.com>
2018-04-26 02:05:06 +0200

    about-text, link-hover, scroll-animation, selection-color

--- app/js/main-min.js
+++ app/js/main-min.js
@@ -1,70 +1,92 @@
-$(window).on('load', function() {
+$(document).ready(function() {
 
     $('.on-load').each(function() {
         $(this).addClass('on-load-ready');
     });
 
+    $('.fadeIn').each(function() {
+        imageScrollAnimation($(this));
+    });
+
+});
+
+
+$(window).on('load', function() {
+
     $(window).scroll(function() {
 
-        var scrollPositionTop = $(window).scrollTop();
-        var header = $('header');
-
-        //console.log(scrollPositionTop);
-        if (scrollPositionTop > 100) {
-            if (header.hasClass('header-top')) {
-                header.addClass('header-scroll');
-                header.removeClass('header-top');
-            }
-        } else {
-            if (header.hasClass('header-scroll')) {
-                header.addClass('header-top');
-                header.removeClass('header-scroll');
-            }
-        }
-
+        headerScrollAnimation();
 
         $('.fadeIn').each(function() {
-            var scrollPositionTop = $(window).scrollTop();
-            var scrollPosition = $(window).scrollTop() + $(window).height();
-            var elementPosition = $(this).offset().top + .5 * $(this).outerHeight();
-
-            if (scrollPositionTop > elementPosition) {
-                if ($(this).hasClass('fadeIn-visible')) {
-                    $(this).removeClass('fadeIn-visible');
-                }
-                if (!$(this).hasClass('fadeOut')) {
-                    $(this).addClass('fadeOut');
-                }
-            } else if (scrollPosition > elementPosition) {
-                if (!$(this).hasClass('fadeIn-visible')) {
-                    $(this).addClass('fadeIn-visible');
-                }
-                if ($(this).hasClass('fadeOut')) {
-                    $(this).removeClass('fadeOut');
-                }
-            } else {
-                if ($(this).hasClass('fadeIn-visible')) {
-                    $(this).removeClass('fadeIn-visible');
-                }
-                if ($(this).hasClass('fadeOut')) {
-                    $(this).removeClass('fadeOut');
-                }
-            }
-
+            imageScrollAnimation($(this));
         });
-
     });
 
 
     var iconRotation = 0;
 
     $(window).mousemove(function() {
-        iconRotation = iconRotation + 5;
+        iconRotation = iconRotation - 5;
 
-        $('.head-icon').each(function() {
-            var sRotate = "rotate(" + iconRotation + "deg)";
-            $(this).css({"-moz-transform" : sRotate, "-webkit-transform" : sRotate});
-        });
+        var kRotate = "rotate(" + iconRotation + "deg)";
+        var rRotate = "rotate(" + (-iconRotation) + "deg)";
+        $('#head-icon--kraut').css({"-moz-transform" : kRotate, "-webkit-transform" : kRotate});
+        $('#head-icon--ruebe').css({"-moz-transform" : rRotate, "-webkit-transform" : rRotate});
     });
 
 });
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
+
+
+// Scroll Functions
+
+function headerScrollAnimation() {
+    var scrollPositionTop = $(window).scrollTop();
+    var header = $('header');
+    if (scrollPositionTop > 100) {
+        if (header.hasClass('header-top')) {
+            header.addClass('header-scroll');
+            header.removeClass('header-top');
+        }
+    } else {
+        if (header.hasClass('header-scroll')) {
+            header.addClass('header-top');
+            header.removeClass('header-scroll');
+        }
+    }
+}
+
+function imageScrollAnimation(object) {
+    var scrollPositionTop = $(window).scrollTop();
+    var scrollPosition = $(window).scrollTop() + $(window).height();
+    var elementPosition = object.offset().top + .5 * object.outerHeight();
+
+    if (scrollPositionTop > elementPosition) {
+        if (object.hasClass('fadeIn-visible')) {
+            object.removeClass('fadeIn-visible');
+        }
+        if (!object.hasClass('fadeOut')) {
+            object.addClass('fadeOut');
+        }
+    } else if (scrollPosition > elementPosition) {
+        if (!object.hasClass('fadeIn-visible')) {
+            object.addClass('fadeIn-visible');
+        }
+        if (object.hasClass('fadeOut')) {
+            object.removeClass('fadeOut');
+        }
+    } else {
+        if (object.hasClass('fadeIn-visible')) {
+            object.removeClass('fadeIn-visible');
+        }
+        if (object.hasClass('fadeOut')) {
+            object.removeClass('fadeOut');
+        }
+    }
+}
