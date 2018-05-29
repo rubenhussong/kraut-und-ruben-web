22ccba619d91d3bd36cef0878f629047c515a687
Florian Ortlieb <nearamarth@gmail.com>
2018-05-29 16:39:37 +0200

    imprint css, php

--- app/php/modules/imprint.php
+++ app/php/modules/imprint.php
@@ -1,16 +1,20 @@
 <section id="imprint">
+<div class="imprint-header">
 	<h1>Impressum</h1>
 	<p>
-		Angaben gemäß § 5 TMG
+		Verantwortlich gemäß § 5 <span class="small-caps">TMG</span>
 	</p>
 	<p>
-		<span>Kraut und Ruben [Ruben Hussong, Florian Ortlieb]</span>
+		<span>Kraut & Ruben</span> <br>
+		<span>Ruben Hussong, Florian Ortlieb</span> <br>
 		<span>Wessenbergstr. 13, 78462 Konstanz</span>
 	</p>
 	<p>
-		<span>Mail: gutentag@krautundruben.com</span> <br>
-		<span>Telefon: +49 157 382 070 73</span>
+		<span>gutentag@krautundruben.com</span> <br>
+		<span>+49 157 382 070 73</span> <br>
 	</p>
+</div>
+<div class="imprint-column" id="imprint-column--left">
 
 	<h2>Haftung für Inhalte</h2>
 	<p>
@@ -21,7 +25,8 @@
 	<p>
 		Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
 	</p>
-
+</div>
+<div class="imprint-column" id="imprint-column--right">
 	<h2>Urheberrecht</h2>
 	<p>
 		Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
@@ -37,4 +42,5 @@
 	<p>
 		Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
 	</p>
+</div>
 </section>
