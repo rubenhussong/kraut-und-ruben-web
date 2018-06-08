c6fb4736e3235501453f5220776bc23662ff8af7
Ruben Hussong <rubenhussong@gmail.com>
2018-06-08 17:56:52 +0200

    small fixes
    scroll up, imprint transition

--- app/php/modules/header.php
+++ app/php/modules/header.php
@@ -17,13 +17,13 @@
         </div>
     </div>
     <div class="page-header" id="header--page-project">
-        <a class="arrow-switch-project header-element header-element--left" href="#page--prev-project" id="arrow--prev-project">
+        <a class="arrow-switch-project header-element header-element--left" href="#page--prev-project" title="Previous Project" id="arrow--prev-project">
             <p>&#x2190;</p>
         </a>
-        <a class="arrow-switch-project header-element header-element--center" href="#page--main" id="arrow--page-main">
+        <a class="arrow-switch-project header-element header-element--center" href="#page--main" title="Main Page" id="arrow--page-main">
             <p>&#x2191;</p>
         </a>
-        <a class="arrow-switch-project header-element header-element--right" href="#page--next-project" id="arrow--next-project">
+        <a class="arrow-switch-project header-element header-element--right" href="#page--next-project" title="Next Project" id="arrow--next-project">
             <p>&#x2192;</p>
         </a>
     </div>
