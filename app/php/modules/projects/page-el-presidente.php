79720de9914a571f0f8a83c9b00bdf2d45f68700
Ruben Hussong <rubenhussong@gmail.com>
2018-06-12 12:52:06 +0200

    el-presidente page content

--- app/php/modules/projects/page-el-presidente.php
+++ app/php/modules/projects/page-el-presidente.php
@@ -6,6 +6,68 @@
         </h2>
     </div>
 </div>
+
+<div class="project-hero scroll-animation" id="project-hero--el-presidente">
+    <img src="" data-src="/img/el-presidente/el-presidente-03.jpg" alt="el-presidente-hero" sizes="auto">
+</div>
+
+<div class="project-description scroll-animation">
+    <p>
+        <?php if ($lang == 'de'): ?>
+            Das Casino Bregenz am Bodensee ist ein architektonisches Meisterwerk der Extrasahne. Seine durch mehr als 14.000 LEDs bespielte Außenfassade fasziniert jeden Tag tausende von Passanten. In Kooperation mit Tom Hegen entstand eine Animation, die gleich mal den Casino Capix Award abräumte.
+        <?php else: ?>
+            Das Casino Bregenz am Bodensee ist ein architektonisches Meisterwerk der Extrasahne. Seine durch mehr als 14.000 LEDs bespielte Außenfassade fasziniert jeden Tag tausende von Passanten. In Kooperation mit Tom Hegen entstand eine Animation, die gleich mal den Casino Capix Award abräumte.
+        <?php endif ?></p>
+</div>
+
+<div class="project-images">
+    <div class="img-container">
+        <div class="img-wrapper img-middle left scroll-animation">
+            <img src="" data-src="/img/el-presidente-banner.jpg" alt="El Presidente Analog Player" sizes="auto">
+            <p>Staunende Passanten schmelzen in einer Symbiose der Emotionen.</p>
+        </div>
+        <div class="img-wrapper img-small right scroll-animation">
+            <img src="" data-src="/img/el-presidente/el-presidente-04.jpg" alt="El Presidente One Day VR" sizes="auto">
+            <p>Volle Kanne im Prozess! Credits to Tom Hegen</p>
+        </div>
+    </div>
+    <div class="img-wrapper img-large scroll-animation">
+        <img src="" data-src="/img/el-presidente/el-presidente-05.jpg" alt="El Presidente VR Player" sizes="auto">
+        <p>Farbenspiel und geile Dynamik</p>
+    </div>
+    <div class="img-container">
+        <div class="img-wrapper img-small left scroll-animation">
+            <img src="" data-src="/img/el-presidente/el-presidente-06.gif" alt="El Presidente VR Twitter Bird" sizes="auto">
+            <p>Ein heimlicher Blick hinter die Fassade</p>
+        </div>
+        <div class="img-wrapper img-middle right scroll-animation">
+            <img src="" data-src="/img/el-presidente/el-presidente-01.jpg" alt="El Presidente Analog Game" sizes="auto">
+            <p>Volle Kanne im Prozess! Credits to Tom Hegen</p>
+        </div>
+    </div>
+    <div class="image-description left scroll-animation">
+        <p>
+            <?php if ($lang == 'de'): ?>
+                Our concept and key idea was to dissolve the borders between the casino and square around the theatre by taking the pedestrians straight onto the wall of the casino. When you’re outside of the building you get a sort of a feeling that you can look through the façade into the casino and see, what is happening there.
+            <?php else: ?>
+                Our concept and key idea was to dissolve the borders between the casino and square around the theatre by taking the pedestrians straight onto the wall of the casino. When you’re outside of the building you get a sort of a feeling that you can look through the façade into the casino and see, what is happening there.
+            <?php endif ?>
+        </p>
+    </div>
+    <div class="img-wrapper img-middle right scroll-animation">
+        <img src="" data-src="/img/el-presidente/el-presidente-07.gif" alt="El Presidente VR Twitter Bird" sizes="auto">
+        <p>Ein heimlicher Blick hinter die Fassade</p>
+    </div>
+    <div class="img-wrapper img-large right scroll-animation">
+        <img src="" data-src="/img/el-presidente/el-presidente-02.jpg" alt="El Presidente Analog Game" sizes="auto">
+        <p>Volle Kanne im Prozess! Credits to Tom Hegen</p>
+    </div>
+    <div class="img-wrapper img-large scroll-animation">
+        <img src="" data-src="/img/el-presidente/el-presidente-03.jpg" alt="El Presidente Video" sizes="auto">
+        <p>Und noch ein schniekes Video</p>
+    </div>
+</div>
+
 <footer class="section change-color" id="section--footer">
     <?php include 'php/modules/footer.php'; ?>
 </footer>
